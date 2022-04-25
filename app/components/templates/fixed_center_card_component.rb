93051447e6bb6d6de5c93c3883e68ad57c7bb705
Eric Roos <eric.roos@zendesk.com>
2022-04-24 19:33:12 -0500

    use components on devise screens

---
+++ app/components/templates/fixed_center_card_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Templates::FixedCenterCardComponent < ViewComponent::Base
+  
+end
---
+++ spec/components/templates/fixed_center_card_component_stories.rb
@@ -0,0 +1,5 @@
+class Templates::FixedCenterCardComponentStories < ViewComponent::Storybook::Stories
+  story :two_columns do
+    constructor
+  end
+end

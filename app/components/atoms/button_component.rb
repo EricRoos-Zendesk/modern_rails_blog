0dcde50fe40a8cff44388ca0e32fe4093bcf194d
Eric Roos <eric.roos@zendesk.com>
2022-04-26 17:11:58 -0500

    cleanup devise views

--- app/components/atoms/button_component.rb
+++ app/components/atoms/button_component.rb
@@ -7,7 +7,7 @@ class Atoms::ButtonComponent < ViewComponent::Base
   end
 
   def self.button_class
-    "rounded-lg py-3 px-5 bg-blue-600 text-white font-medium flex items-center justify-center"
+    "cursor-pointer rounded-lg py-3 px-5 bg-blue-600 text-white font-medium flex items-center justify-center hover:brightness-90 transition-all"
   end
 
   def button_class
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   default_form_builder CustomFormBuilder
+  before_action :authenticate_user!
 end
